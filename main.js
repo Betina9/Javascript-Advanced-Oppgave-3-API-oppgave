a54e9d70e5c73f3aea4b78d15b859a460b07901c
Betina9 <ob042025@gmail.com>
2025-08-07 00:22:07 +0200

    Satt inn bilde av katter fra api

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const button = document.getElementById("button");
 const display = document.getElementById("display");
+const image = document.getElementById("cat-image");
 
 async function getData() {
-  const result = await fetch("https://meowfacts.herokuapp.com/");
+  try {
+    const result = await fetch("https://meowfacts.herokuapp.com/");
+    const data = await result.json();
+    display.textContent = data.data[0];
 
-  const data = await result.json();
-  display.textContent = data.data[0];
+    const randomParam = Date.now(); // gjør hver URL unik
+    image.src = `https://cataas.com/cat?timestamp=${randomParam}`;
+  } catch (error) {
+    display.textContent = "Kunne ikke hente fakta 😿";
+    console.error(error);
+  }
 }
+
 button.addEventListener("click", () => {
   getData();
 });
