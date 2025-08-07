75f520c130242a349ae752e98f4d501e19f796f6
Betina9 <ob042025@gmail.com>
2025-08-07 12:34:16 +0200

    Fikk mute knappen til å funke

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const button = document.getElementById("button");
+const muteBtn = document.getElementById("mute");
 const display = document.getElementById("display");
 const image = document.getElementById("cat-image");
 const sound = document.getElementById("meow-sound");
+
+let isMuted = false;
 
 async function getData() {
   try {
@@ -11,6 +14,11 @@ async function getData() {
 
     const randomParam = Date.now(); // gjør hver URL unik
     image.src = `https://cataas.com/cat?timestamp=${randomParam}`;
+
+    if (isMuted) {
+      sound.currentTime = 0;
+      sound.play();
+    }
   } catch (error) {
     display.textContent = "Kunne ikke hente fakta 😿";
     console.error(error);
@@ -23,3 +31,9 @@ button.addEventListener("click", () => {
 });
 
 window.addEventListener("DOMContentLoaded", getData);
+
+muteBtn.addEventListener("click", () => {
+  isMuted = !isMuted;
+  sound.muted = isMuted;
+  muteBtn.textContent = isMuted ? "🔇" : "🔊";
+});
